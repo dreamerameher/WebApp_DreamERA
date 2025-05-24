e474141df82ad73928e7eb8b9938edfd06582c75
srushti kashetwar <srushtikashetwar@gmail.com>
2025-05-24 16:30:04 +0530

    initial_commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp_DreamERA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
